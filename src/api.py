ac7abab970a7c18436b66b5504c3388c4807ef0e
BMueller11 <b-m.mueller@gmx.de>
2022-06-30 14:48:08 +0200

    add api and wsgi file

---
+++ src/api.py
@@ -0,0 +1,12 @@
+from flask import Flask, Response, request
+import pandas as pd
+import os
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return{"hello": "world"}
