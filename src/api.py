abded24218ab4cecc9f8832c5b04afb4d119ad31
BMueller11 <b-m.mueller@gmx.de>
2022-06-30 15:07:37 +0200

    DIG-490 add api ressources

--- src/api.py
+++ src/api.py
@@ -1,3 +1,4 @@
+import mimetypes
 from flask import Flask, Response, request
 import pandas as pd
 import os
@@ -6,7 +7,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+training_data = pd.read_csv(os.path.join('data', 'auto-mpg.csv'),sep=";")
 
 @app.route("/", methods=["GET"])
 def index():
     return{"hello": "world"}
+
+
+@app.route("/hello_world", methods=["GET"])
+def hello_world():
+    return "<p>hello World\<p>"
+
+
+@app.route("/training_data", methods=["GET"])
+def get_training_data():
+    return Response(training_data.to_json(), mimetype="application/json")
